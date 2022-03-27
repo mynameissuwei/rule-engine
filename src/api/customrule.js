768ac648c6ffeee4a44ed416432e5fe59e4891c7
suwei <suwei@suweideMacBook-Pro.local>
2022-03-27 12:36:20 +0800

    fix: 默认忽略大小写

---
+++ src/api/customrule.js
@@ -0,0 +1,31 @@
+import request from '@/utils/request'
+
+export const fetchTableData = ({ data }) => {
+  return request({
+    url: '/rule/manager/pageList',
+    method: 'post',
+    data
+  })
+}
+
+export const deleteList = (id) => {
+  return request({
+    url: `/rule/manager/deleteOneRule/${id}`,
+    method: 'get'
+  })
+}
+
+export const modifyList = (data) => {
+  return request({
+    url: `/rule/manager/batchChangeRules`,
+    method: 'post',
+    data
+  })
+}
+
+export const fetchDetail = (id) => {
+  return request({
+    url: `/rule/manager/viewOneRule/${id}`,
+    method: 'get'
+  })
+}
