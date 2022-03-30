be4484654f19c661b7fd55c43fc19fafcb2dc80f
suwei <suwei@suweideMacBook-Pro.local>
2022-03-30 14:32:16 +0800

    fix: main.js

--- src/main.js
+++ src/main.js
@@ -7,16 +7,20 @@ import { Base64 } from "js-base64";
 import EnnAuthSdk from "@enncloud/enn-auth-sdk/src/index";
 import element from "plugins/element";
 
-EnnAuthSdk({
-  baseUrl: import.meta.env.VITE_APP_LOGIN_API, //项目后端接口地址
-  appid: import.meta.env.VITE_APP_APP_ID, // 项目APPID
-  accessKey: import.meta.env.VITE_APP_ACCESS_KEY, // 网关key
-  authUrl: import.meta.env.VITE_APP_AUTH_API,
-  env: import.meta.env.VITE_APP_ENV, // 当前运行环境 develop or production
-  warningCb: (message) => {
-    console.log(message, "error");
-  },
-}).then((ennAuth) => {
+// EnnAuthSdk({
+//   baseUrl: import.meta.env.VITE_APP_LOGIN_API, //项目后端接口地址
+//   appid: import.meta.env.VITE_APP_APP_ID, // 项目APPID
+//   accessKey: import.meta.env.VITE_APP_ACCESS_KEY, // 网关key
+//   authUrl: import.meta.env.VITE_APP_AUTH_API,
+//   env: import.meta.env.VITE_APP_ENV, // 当前运行环境 develop or production
+//   warningCb: (message) => {
+//     console.log(message, "error");
+//   },
+// }).then((ennAuth) => {
+//   initVue(ennAuth);
+// });
+
+const initVue = (authSdk) => {
   const app = createApp(App);
   app.config.globalProperties.$ennAuth = authSdk;
   app.use(router);
@@ -25,3 +29,5 @@ EnnAuthSdk({
   app.use(element);
   app.mount("#app");
 };
+
+initVue("");
