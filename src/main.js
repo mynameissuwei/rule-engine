da667eecb83e30336838dce1ec5e124cb5749130
wuleip <wuleip@enn.cn>
2022-04-01 11:58:32 +0800

    恢复authsdk

--- src/main.js
+++ src/main.js
@@ -7,18 +7,18 @@ import { Base64 } from "js-base64";
 import EnnAuthSdk from "@enncloud/enn-auth-sdk/src/index";
 import element from "plugins/element";
 
-// EnnAuthSdk({
-//   baseUrl: import.meta.env.VITE_APP_LOGIN_API, //项目后端接口地址
-//   appid: import.meta.env.VITE_APP_APP_ID, // 项目APPID
-//   accessKey: import.meta.env.VITE_APP_ACCESS_KEY, // 网关key
-//   authUrl: import.meta.env.VITE_APP_AUTH_API,
-//   env: import.meta.env.VITE_APP_ENV, // 当前运行环境 develop or production
-//   warningCb: (message) => {
-//     console.log(message, "error");
-//   },
-// }).then((ennAuth) => {
-//   initVue(ennAuth);
-// });
+EnnAuthSdk({
+  baseUrl: import.meta.env.VITE_APP_LOGIN_API, //项目后端接口地址
+  appid: import.meta.env.VITE_APP_APP_ID, // 项目APPID
+  accessKey: import.meta.env.VITE_APP_ACCESS_KEY, // 网关key
+  authUrl: import.meta.env.VITE_APP_AUTH_API,
+  env: import.meta.env.VITE_APP_ENV, // 当前运行环境 develop or production
+  warningCb: (message) => {
+    console.log(message, "error");
+  },
+}).then((ennAuth) => {
+  initVue(ennAuth);
+});
 
 const initVue = (authSdk) => {
   const app = createApp(App);
