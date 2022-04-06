7968fbdefb62196693c03ceb7c4126eef1e450fa
施QAQ越 <2667128421@qq.com>
2022-04-06 21:09:42 +0800

    脚本规则编辑页面

--- src/views/ruleRepository/updateRuleRepository.vue
+++ src/views/ruleRepository/updateRuleRepository.vue
@@ -96,7 +96,6 @@ export default {
                 description: updateRuleRepositoryForm.form.RuleRepositoryDescription
               }
             })
-                getRuleRepository
           }
       )
 
