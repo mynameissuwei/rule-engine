5b2d2bfdfe8c63dc6db7b183cb0377f19132ccbe
suwei <suwei@suweideMacBook-Pro.local>
2022-04-24 15:41:29 +0800

    fix: 去除鉴权逻辑

--- src/main.js
+++ src/main.js
@@ -4,30 +4,32 @@ import router from "@/router";
 import store from "@/store";
 import "styles/index.scss";
 import { Base64 } from "js-base64";
-import EnnAuthSdk from "@enncloud/enn-auth-sdk/src/index";
+// import EnnAuthSdk from "@enncloud/enn-auth-sdk/src/index";
 import element from "plugins/element";
 import "@/permission"; // 权限控制
 
-EnnAuthSdk({
-  baseUrl: import.meta.env.VITE_APP_LOGIN_API, //项目后端接口地址
-  appid: import.meta.env.VITE_APP_APP_ID, // 项目APPID
-  accessKey: import.meta.env.VITE_APP_ACCESS_KEY, // 网关key
-  authUrl: import.meta.env.VITE_APP_AUTH_API,
-  env: import.meta.env.VITE_APP_ENV, // 当前运行环境 develop or production
-  warningCb: (message) => {
-    console.log(message, "error");
-  },
-}).then((ennAuth) => {
-  initVue(ennAuth);
-});
+// EnnAuthSdk({
+//   baseUrl: import.meta.env.VITE_APP_LOGIN_API, //项目后端接口地址
+//   appid: import.meta.env.VITE_APP_APP_ID, // 项目APPID
+//   accessKey: import.meta.env.VITE_APP_ACCESS_KEY, // 网关key
+//   authUrl: import.meta.env.VITE_APP_AUTH_API,
+//   env: import.meta.env.VITE_APP_ENV, // 当前运行环境 develop or production
+//   warningCb: (message) => {
+//     console.log(message, "error");
+//   },
+// }).then((ennAuth) => {
+//   initVue(ennAuth);
+// });
 
-const initVue = (authSdk) => {
+const initVue = () => {
   // authSdk.logout();
   const app = createApp(App);
-  app.config.globalProperties.$ennAuth = authSdk;
+  // app.config.globalProperties.$ennAuth = authSdk;
   app.use(router);
   app.use(store);
   app.use(Base64);
   app.use(element);
   app.mount("#app");
 };
+
+initVue();
